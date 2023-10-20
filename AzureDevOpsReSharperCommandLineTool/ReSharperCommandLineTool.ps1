d7a60d0e1c3cd686fde7c536b4df4480c6bd80cb
Gerard Groenewegen <g.groenewegen@nitea.nl>
2023-10-20 21:48:44 +0200

    Added powershell script

--- AzureDevOpsReSharperCommandLineTool/ReSharperCommandLineTool.ps1
+++ AzureDevOpsReSharperCommandLineTool/ReSharperCommandLineTool.ps1
@@ -1,18 +1,100 @@
-[CmdletBinding()]
-param()
+[string]$inspectCodeTarget  = Get-VstsInput -Name Target
+$inspectCodeToolFolder = "resharper_commandline_tools"
+$inspectCodeResultsPath  = "$($inspectCodeToolFolder)\report.xml"
+$inspectCodeResultsHtmlPath  = "$($inspectCodeToolFolder)\report.html"
+$summaryFilePath  = "$($inspectCodeToolFolder)\Summary.md"
 
-# For more information on the Azure DevOps Task SDK:
-# https://github.com/Microsoft/vsts-task-lib
-Trace-VstsEnteringInvocation $MyInvocation
-try {
-    # Set the working directory.
-    $cwd = Get-VstsInput -Name cwd -Require
-    Assert-VstsPath -LiteralPath $cwd -PathType Container
-    Write-Verbose "Setting working directory to '$cwd'."
-    Set-Location $cwd
+Write-Output "Install ReSharper CommandLine Tools"
+mkdir -p $inspectCodeToolFolder
+& nuget install JetBrains.ReSharper.CommandLineTools -OutputDirectory $inspectCodeToolFolder
 
-    # Output the message to the log.
-    Write-Host (Get-VstsInput -Name msg)
-} finally {
-    Trace-VstsLeavingInvocation $MyInvocation
+Write-Output "Run Inspect Code"
+& ".\$inspectCodeToolFolder\JetBrains.ReSharper.CommandLineTools*\tools\inspectcode.exe" $inspectCodeTarget "--output=$($inspectCodeToolFolder)\" "--format=Html;Xml" "/disable-settings-layers:SolutionPersonal" "--build" "--properties:Configuration=Release"
+
+Write-Output "Analyse Results"
+$severityLevels = @{"HINT" = 0; "SUGGESTION" = 1; "WARNING" = 2; "ERROR" = 3}
+$severityLevelSuggestion = $severityLevels["SUGGESTION"]
+$severityLevelWarning = $severityLevels["WARNING"]
+$severityLevelError = $severityLevels["ERROR"]
+$failBuildLevelSelector = "ERROR"
+$failBuildLevelSelectorValue = $severityLevels[$failBuildLevelSelector]
+
+$xmlContent = [xml] (Get-Content "$inspectCodeResultsPath")
+$issuesTypesXpath = "/Report/IssueTypes//IssueType"
+$issuesTypesElements = $xmlContent | Select-Xml $issuesTypesXpath | Select -Expand Node
+$issuesTypesElementsPSObject = @{}
+foreach($issuesTypesElement in $issuesTypesElements) {
+    $issuesTypesElementsPSObject.Add($issuesTypesElement.Attributes["Id"].Value, $issuesTypesElement.Attributes["Severity"].Value)
 }
+
+$issuesXpath = "/Report/Issues//Issue"
+$issuesElements = $xmlContent | Select-Xml $issuesXpath | Select-Object -Expand Node
+$issuesElementsPSObject = [System.Collections.ArrayList]::new()
+foreach($issuesElement in $issuesElements) {
+    $typeId = $issuesElement.Attributes["TypeId"].Value
+    $severity = $issuesTypesElementsPSObject[$typeId]
+    if($severity -eq "INVALID_SEVERITY") {
+        $severity = $issuesElement.Attributes["Severity"].Value
+    }
+    $severityLevel = $severityLevels[$severity]
+    $item = [PSCustomObject] @{
+        TypeId = $typeId
+        Severity = $severity
+        SeverityLevel = $severityLevel
+        Message = $issuesElement.Attributes["Message"].Value
+        File = $issuesElement.Attributes["File"].Value
+        Line = $issuesElement.Attributes["Line"].Value
+        }
+    $null = $issuesElementsPSObject.Add($item)
+}
+
+$filteredElementsFail = [System.Collections.ArrayList]::new()
+$filteredElementsReportSuggestion = 0
+$filteredElementsReportWarning = 0
+$filteredElementsReportError = 0
+foreach($issue in $issuesElementsPSObject) {
+    if($issue.SeverityLevel -ge $failBuildLevelSelectorValue) {
+        $item = New-Object -TypeName PSObject -Property @{
+            'Severity' = $severity
+            'Message' = $issue.Message
+            'File' = $issue.File
+            'Line' = $issue.Line
+        }
+        $null = $filteredElementsFail.Add($item)
+    }
+    if($issue.SeverityLevel -eq $severityLevelSuggestion) {
+        $filteredElementsReportSuggestion++
+    }
+    if($issue.SeverityLevel -eq $severityLevelWarning) {
+        $filteredElementsReportWarning++
+    }
+    if($issue.SeverityLevel -eq $severityLevelError) {
+        $filteredElementsReportError++
+    }
+}
+
+Write-Output "Report Results Output"
+foreach ($issue in $filteredElementsFail | Sort-Object Severity -Descending) {
+    $errorType = "warning"
+    if($issue.Severity -eq "ERROR"){
+        $errorType = "error"
+    }
+    Write-Output ("##vso[task.logissue type={0};sourcepath={1};linenumber={2};columnnumber=1]R# {3}" -f $errorType, $issue.File, $issue.Line, $issue.Message)
+}
+
+$null = New-Item $summaryFilePath -type file -force
+$summaryMessage = "Code inspect found $($filteredElementsReportSuggestion) suggestions, $($filteredElementsReportWarning) warnings and $($filteredElementsReportError) errors"
+Write-Output $summaryMessage
+Add-Content $summaryFilePath ($summaryMessage)
+
+Write-Output "##vso[artifact.upload containerfolder=inspect_code;artifactname=inspect_code]$([IO.Path]::GetFullPath($summaryFilePath))"
+Write-Output "##vso[artifact.upload containerfolder=inspect_code;artifactname=inspect_code]$([IO.Path]::GetFullPath($inspectCodeResultsPath))"
+Write-Output "##vso[artifact.upload containerfolder=inspect_code;artifactname=inspect_code]$([IO.Path]::GetFullPath($inspectCodeResultsHtmlPath))"
+Write-Output "##vso[task.addattachment type=Distributedtask.Core.Summary;name=Resharper Command Line Tools Inspect Code;]$([IO.Path]::GetFullPath($summaryFilePath))"
+
+Remove-Item $inspectCodeToolFolder -Recurse -Force
+$buildResult = "Succeeded"
+if($filteredElementsFail.Count -gt 0) {
+    $buildResult = "Failed"
+}
+Write-Output ("##vso[task.complete result={0};]{1}" -f $buildResult, $summaryMessage)
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Azure DevOps Pipelines Extension for using the ReSharper command line tools.
 https://www.jetbrains.com/help/resharper/ReSharper_Command_Line_Tools.html
+
+Useful links
+https://4bes.nl/2021/02/21/create-a-custom-azure-devops-powershell-task/
+https://github.com/Ba4bes/AzDoPassword-Generator/
